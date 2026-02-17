62e307b3dfe76befccfe1d4e1fa365d83a227d08
chen3 <chen316487@gmail.com>
2026-02-17 11:40:19 +0200

    Add run_tests.h to enable running all tests from main

---
+++ include/run_tests.h
@@ -0,0 +1,15 @@
+#ifndef RUN_TESTS_H
+#define RUN_TESTS_H
+
+/**
+ * @brief Runs all unit tests in the system.
+ *
+ * This function executes tests for the main components of the system:
+ * - runStateTests() – tests the system's state management
+ * - runWorldTests() – tests the world representation
+ * - runGraphTests() – tests graph structures and algorithms
+ * - runPlannerTests() – tests the planning functionality
+ */
+void runAllTests();
+
+#endif // RUN_TESTS_H
--- tests/run_tests.cpp
+++ tests/run_tests.cpp
@@ -1,10 +1,11 @@
+#include "run_tests.h"
 #include "test_state.cpp"
 #include "test_world.cpp"
 #include "test_graph.cpp"
 #include "test_planner.cpp"
 #include "test_framework.h"
 
-int main()
+void runAllTests()
 {
     runStateTests();
     runWorldTests();
@@ -12,6 +13,4 @@ int main()
     runPlannerTests();
 
     printSummary();
-
-    return 0;
 }
