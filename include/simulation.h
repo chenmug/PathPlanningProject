13718b53cb80d9251bb2d08e8e9f95ad89f5e0b2
chen3 <chen316487@gmail.com>
2026-02-18 22:10:21 +0200

    Simulation: Add compareAlgorithm function

--- include/simulation.h
+++ include/simulation.h
@@ -16,7 +16,7 @@
  * - Run the planner and get the path
  * - Animate the agent moving along the path
  * - Display grid and path in console
- * - Show stats (steps, cost, success)
+ * - Show stats (steps, cost, success, time)
  */
 class Simulation
 {
@@ -33,31 +33,41 @@ private:
      * @brief Fills the world with random obstacles.
      * 
      * @param obstaclePercentage Percentage of blocked cells
+     * @param type The search algorithm type (BFS, Dijkstra, or A*) used to decide weight assignment
      */
     void generateRandomObstacles(int obstaclePercentage, SearchType type);
 
-
-    /**
-     * @brief Prints the current grid to the console.
-     *        Shows agent position, goal, obstacles, and path.
-     * 
-     * @param agentPos Current position of the agent
-     * @param path Optional path to highlight (can show arrows or asterisks)
-     */
-    void displayGrid(const State& agentPos, const std::vector<State>& path, SearchType type, int step, int points) const;
-
 public:
     /**
-     * @brief Constructs a simulation with given grid size and start/goal positions.
+     * @brief Constructs a simulation with a given grid size and start/goal positions.
+     *
+     * @param width The width of the grid
+     * @param height The height of the grid
+     * @param start The starting position of the agent
+     * @param goal The goal position the agent needs to reach
      */
     Simulation(int width, int height, const State& start, const State& goal);
 
     /**
      * @brief Runs a single simulation in the console.
-     *        Allows selecting the algorithm, generates obstacles,
-     *        computes the path, animates the agent, and prints stats.
+     *        Allows the user to select the algorithm, computes the path
+     *        animates the agent, and prints stats.
+     *
+     * @param type The search algorithm to use (BFS, Dijkstra, or A*)
      */
     void run(SearchType type);
+
+    /**
+     * @brief Compares the performance of different algorithms (BFS, Dijkstra, A*) on the same grid.
+     *
+     * This function executes each algorithm (BFS, Dijkstra, A*) on a randomly generated world and compares:
+     * - The time taken to compute the path
+     * - The number of nodes expanded during the search
+     * - The computed path and its cost
+     *
+     * The goal is to provide insights into the efficiency and optimality of each algorithm in various scenarios.
+     */
+    void compareAlgorithms();
 };
 
 #endif // SIMULATION_H
